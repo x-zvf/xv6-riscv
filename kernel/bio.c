fd06b87c6cbacc53c3f9fd56370112d6f4d2d2ce
Morridin <paulza@gmx.de>
2023-05-15 18:14:50 +0200

    Changed buf.data to a page pointer.
    Also brought back the locks in bio.c.
    Things are not working atm.

--- kernel/bio.c
+++ kernel/bio.c
@@ -18,7 +18,7 @@
 #include "buf.h"
 
 struct {
-//  struct spinlock lock;
+  struct spinlock lock;
   struct buf buf[NBUF];
 
   // Linked list of all buffers, through prev/next.
@@ -32,7 +32,7 @@ binit(void)
 {
   struct buf *b;
 
-//  initlock(&bcache.lock, "bcache");
+  initlock(&bcache.lock, "bcache");
 
   // Create linked list of buffers
   bcache.head.prev = &bcache.head;
@@ -40,7 +40,8 @@ binit(void)
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
     b->next = bcache.head.next;
     b->prev = &bcache.head;
-//    initsleeplock(&b->lock, "buffer");
+    b->data = (uchar**) kalloc();
+    initsleeplock(&b->lock, "buffer");
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
@@ -54,14 +55,14 @@ bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-//  acquire(&bcache.lock);
+  acquire(&bcache.lock);
 
   // Is the block already cached?
   for(b = bcache.head.next; b != &bcache.head; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-//      release(&bcache.lock);
-//      acquiresleep(&b->lock);
+      release(&bcache.lock);
+      acquiresleep(&b->lock);
       return b;
     }
   }
@@ -74,8 +75,8 @@ bget(uint dev, uint blockno)
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
-//      release(&bcache.lock);
-//      acquiresleep(&b->lock);
+      release(&bcache.lock);
+      acquiresleep(&b->lock);
       return b;
     }
   }
@@ -100,8 +101,8 @@ bread(uint dev, uint blockno)
 void
 bwrite(struct buf *b)
 {
-//  if(!holdingsleep(&b->lock))
-//    panic("bwrite");
+  if(!holdingsleep(&b->lock))
+    panic("bwrite");
   virtio_disk_rw(b, 1);
 }
 
@@ -110,12 +111,12 @@ bwrite(struct buf *b)
 void
 brelse(struct buf *b)
 {
-//  if(!holdingsleep(&b->lock))
-//    panic("brelse");
+  if(!holdingsleep(&b->lock))
+    panic("brelse");
 
-//  releasesleep(&b->lock);
+  releasesleep(&b->lock);
 
-//  acquire(&bcache.lock);
+  acquire(&bcache.lock);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
@@ -127,19 +128,19 @@ brelse(struct buf *b)
     bcache.head.next = b;
   }
   
-//  release(&bcache.lock);
+  release(&bcache.lock);
 }
 
 void
 bpin(struct buf *b) {
-//  acquire(&bcache.lock);
+  acquire(&bcache.lock);
   b->refcnt++;
-//  release(&bcache.lock);
+  release(&bcache.lock);
 }
 
 void
 bunpin(struct buf *b) {
-//  acquire(&bcache.lock);
+  acquire(&bcache.lock);
   b->refcnt--;
-//  release(&bcache.lock);
+  release(&bcache.lock);
 }
