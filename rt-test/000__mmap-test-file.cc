517dd90ad912a07c8f48961d4487184563a1ce74
Morridin <paulza@gmx.de>
2023-06-09 11:35:54 +0200

    Removed an F printing

--- rt-test/000__mmap-test-file.cc
+++ rt-test/000__mmap-test-file.cc
@@ -9,16 +9,15 @@
 #include <kernel/fcntl.h>
 
 void main() {
-  write(1, "F",1);
   printf("Begin test\n");
   //test invalid FD
-  char *data = (char*) mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, 122, 0);
+  char *data = (char *)mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, 122, 0);
   assert(data == MAP_FAILED);
 
   // opened file
   int fd = open("./foo.txt", O_CREATE | O_RDWR);
   assert(fd >= 0);
-  data = (char*) mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  data = (char *)mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   printf("mmap'ed file at %p\n", data);
   assert(data != MAP_FAILED);
   data[0] = 't';
@@ -27,12 +26,12 @@ void main() {
   data[3] = 't';
   printf("Here");
   munmap(data, 4096);
-//  data = (char*) mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-//  assert(data[0] == 't');
-//  assert(data[1] == 'e');
-//  assert(data[2] == 's');
-//  assert(data[3] == 't');
-//  munmap(data, 4096);
+  //  data = (char*) mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  //  assert(data[0] == 't');
+  //  assert(data[1] == 'e');
+  //  assert(data[2] == 's');
+  //  assert(data[3] == 't');
+  //  munmap(data, 4096);
   close(fd);
   fd = open("./foo.txt", O_CREATE | O_RDWR);
   char buf[4096];
